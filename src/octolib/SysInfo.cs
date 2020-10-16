0e2794d8a17759582ebaca10682648f5b203d726
Brian A. Randell <brianr@mcwtech.com>
2020-10-16 03:42:52 -0700

    Add simple method

---
+++ src/octolib/SysInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace octolib
+{
+    public static class SysInfo
+    {
+        public static string GetMachineInfoString()
+        {
+            return $"{Environment.MachineName} is running {Environment.OSVersion}";
+        }
+    }
+}
